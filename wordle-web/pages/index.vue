d066639ecf64928884621f64081dbd8e4e6f19b1
mpennock5 <mpennock6@outlook.com>
2022-04-09 13:32:49 -0700

    following lecture to debug

--- wordle-web/pages/index.vue
+++ wordle-web/pages/index.vue
@@ -81,11 +81,7 @@
 </template>
 
 <script lang="ts">
-import Vue from "vue"
-import Component from "vue-class-compenent"
-
-@Component
-export default class IndexPage extends Vue{
-  name: string = 'IndexPage'
+export default{
+  name: 'IndexPage'
 }
 </script>
