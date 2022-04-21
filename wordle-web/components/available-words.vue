9dfbff8e18cd17da9f711fc2c8f1e7f931b2be6a
mpennock5 <mpennock6@outlook.com>
2022-04-20 23:42:27 -0700

    getting error: ts thinking function is a property

---
+++ wordle-web/components/available-words.vue
@@ -0,0 +1,19 @@
+
+
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator'
+import { Letter, LetterStatus } from '~/scripts/letter'
+import { WordleGame } from '~/scripts/wordleGame'
+import { WordsService } from '~/scripts/wordsService'
+
+@Component
+export default class KeyBoard extends Vue {
+  @Prop({ required: true })
+  wordleGame!: WordleGame
+  
+
+
+
+}
+</script>
