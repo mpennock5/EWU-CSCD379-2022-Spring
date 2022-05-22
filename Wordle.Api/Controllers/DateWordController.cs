a43f5be799cc1c002f9049b34c632dd7bd15d32e
Matt <mpennock6@outlook.com>
2022-05-22 13:04:59 -0700

    added route to dateword controller

--- Wordle.Api/Controllers/DateWordController.cs
+++ Wordle.Api/Controllers/DateWordController.cs
@@ -28,60 +28,60 @@ public class DateWordController : Controller
 
     //}
 
-    [HttpGet]
-    public string? GetDailyWord(DateTime date)
-    {
-        //Sanitize the date by dropping time data
-        date = date.Date;
-        if (date.ToUniversalTime() >= System.DateTime.Today.ToUniversalTime().AddDays(0.5))
-        {
-            return null;
-        }
-        //Check if the day has a word in the database
-        if (_cache.TryGetValue(date, out var word))
-        {
-            return word;
-        }
-        DateWord? wordOfTheDay = _context.DateWords
-            .Include(x => x.Word)
-            .FirstOrDefault(dw => dw.Date == date);
+    //[HttpGet]
+    //public string? GetDailyWord(DateTime date)
+    //{
+    //    //Sanitize the date by dropping time data
+    //    date = date.Date;
+    //    if (date.ToUniversalTime() >= System.DateTime.Today.ToUniversalTime().AddDays(0.5))
+    //    {
+    //        return null;
+    //    }
+    //    //Check if the day has a word in the database
+    //    if (_cache.TryGetValue(date, out var word))
+    //    {
+    //        return word;
+    //    }
+    //    DateWord? wordOfTheDay = _context.DateWords
+    //        .Include(x => x.Word)
+    //        .FirstOrDefault(dw => dw.Date == date);
 
-        if (wordOfTheDay != null)
-        //Yes: return the word
-        {
-            _cache.TryAdd(date, wordOfTheDay.Word.Value);
-            return wordOfTheDay.Word.Value;
-        }
-        else
-        {
-            //Mutex magic
-            lock (_mutex)
-            {
-                wordOfTheDay = _context.DateWords
-                    .Include(x => x.Word)
-                    .FirstOrDefault(dw => dw.Date == date);
-                if (wordOfTheDay != null)
-                //Yes: return the word
-                {
-                    return wordOfTheDay.Word.Value;
-                }
-                else
-                {
-                    //No: get a random word from our list
-                    var chosenWord = _gameService.GetWord();
-                    //Save the word to the database with the date
-                    _context.DateWords.Add(new DateWord { Date = date, Word = chosenWord });
-                    _context.SaveChanges();
-                    //Return the word
-                    _cache.TryAdd(date, chosenWord.Value);
-                    return chosenWord.Value;
-                }
-            }
-        }
-    }
-
+    //    if (wordOfTheDay != null)
+    //    //Yes: return the word
+    //    {
+    //        _cache.TryAdd(date, wordOfTheDay.Word.Value);
+    //        return wordOfTheDay.Word.Value;
+    //    }
+    //    else
+    //    {
+    //        //Mutex magic
+    //        lock (_mutex)
+    //        {
+    //            wordOfTheDay = _context.DateWords
+    //                .Include(x => x.Word)
+    //                .FirstOrDefault(dw => dw.Date == date);
+    //            if (wordOfTheDay != null)
+    //            //Yes: return the word
+    //            {
+    //                return wordOfTheDay.Word.Value;
+    //            }
+    //            else
+    //            {
+    //                //No: get a random word from our list
+    //                var chosenWord = _gameService.GetWord();
+    //                //Save the word to the database with the date
+    //                _context.DateWords.Add(new DateWord { Date = date, Word = chosenWord });
+    //                _context.SaveChanges();
+    //                //Return the word
+    //                _cache.TryAdd(date, chosenWord.Value);
+    //                return chosenWord.Value;
+    //            }
+    //        }
+    //    }
+    //}
+    [Route("[action]")]
     [HttpGet]
-    public string? GetAnotherDailyWord(int year, int month, int day)
+    public string? GetWordByDate(int year, int month, int day)
     {
         //Sanitize the date by dropping time data
         DateTime date = new DateTime(year, month, day);
