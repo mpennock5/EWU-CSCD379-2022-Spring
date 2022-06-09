d8f89d2022858d46771deda645b849f6c455ab04
DakotaCondos <79940799+DakotaCondos@users.noreply.github.com>
2022-06-09 15:45:19 -0700

    added migration

---
+++ Wordle.Api/Migrations/20220609224447_pagination.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Wordle.Api.Migrations
+{
+    public partial class pagination : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "DateOfBirth",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "MasterOfTheUniverse",
+                table: "AspNetUsers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateOfBirth",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "MasterOfTheUniverse",
+                table: "AspNetUsers");
+        }
+    }
+}
